95242dade4c5fd3facd91d4a61edea5294c09d98
Brijal Panchal <brijalbenpanchal@loyalistcollege.com>
2024-02-23 20:25:04 -0500

    Issue: memory leak issue root cause: in aws sdk init log level is set to debug causing every log is entered causing high memory for logging file. Fix: updated log level from debug to error only

--- samples/s3_storage_plugin/src/daily_loger.hpp
+++ samples/s3_storage_plugin/src/daily_loger.hpp
@@ -16,8 +16,8 @@
 
 namespace fs = std::filesystem;
 
-#define DEBUGLOG(...) ""
-// #define DEBUGLOG(...) nx_spl::aux::DailyLogger::Log(nx_spl::aux::DailyLogger::LogPriority::DebugP, __FUNCTION__, __LINE__, __VA_ARGS__);
+// #define DEBUGLOG(...) ""
+#define DEBUGLOG(...) nx_spl::aux::DailyLogger::Log(nx_spl::aux::DailyLogger::LogPriority::DebugP, __FUNCTION__, __LINE__, __VA_ARGS__);
 #define INFOLOG(...) nx_spl::aux::DailyLogger::Log(nx_spl::aux::DailyLogger::LogPriority::InfoP, __FUNCTION__, __LINE__, __VA_ARGS__);
 #define ERRORLOG(...) nx_spl::aux::DailyLogger::Log(nx_spl::aux::DailyLogger::LogPriority::ErrorP, __FUNCTION__, __LINE__, __VA_ARGS__);
 
