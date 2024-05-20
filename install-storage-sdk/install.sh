261d7ab8e8e9538973eb9d8cabadeb2a15a21b9a
Brijal Panchal <brijalpanchal08@gmail.com>
2024-05-20 11:14:32 -0400

    updated install.sh

--- install-storage-sdk/install.sh
+++ install-storage-sdk/install.sh
@@ -7,6 +7,7 @@ echo "Settingup sdk .."
 
 apt-get install libcurl4-openssl-dev libssl-dev uuid-dev zlib1g-dev libpulse-dev
 cp license.config /opt/networkoptix/mediaserver/bin/
+cp s3.config /opt/networkoptix/mediaserver/bin/
 cp libs3_storage_plugin.so /opt/networkoptix/mediaserver/bin/plugins/
 systemctl restart networkoptix-mediaserver.service
 
