271a06411193b911b321b8d8a847b6af86956bfe
Brijal Panchal <brijalbenpanchal@loyalistcollege.com>
2024-07-19 00:56:28 -0400

    added hanwah oem installation for wondows

--- install-storage-sdk-window/install.bat
+++ install-storage-sdk-window/install.bat
@@ -7,7 +7,7 @@ set BASE_DIR_WITH_BACKSLASH=%~dp0
 set BASE_DIR=%BASE_DIR_WITH_BACKSLASH:~0,-1%
 
 @echo off
-%BASE_DIR%/Storage_SDK_License_Config.exe "DW Spectrum"
+%BASE_DIR%/Storage_SDK_License_Config.exe "Wisenet WAVE"
 pause
 
 set ARTIFACT="license.config"
@@ -16,10 +16,10 @@ if not exist "%ARTIFACT%" (
     exit /b 70
 )
 
-copy %BASE_DIR%"\lib\*" "C:\Program Files\Digital Watchdog\DW Spectrum\MediaServer\"
-copy ".\license.config" "C:\Program Files\Digital Watchdog\DW Spectrum\MediaServer\"
-copy %BASE_DIR%"\s3.config" "C:\Program Files\Digital Watchdog\DW Spectrum\MediaServer\"
-copy %BASE_DIR%"\s3_storage_plugin.dll" "C:\Program Files\Digital Watchdog\DW Spectrum\MediaServer\plugins\"
+copy %BASE_DIR%"\lib\*" "C:\Program Files\Hanwha\Wisenet WAVE\MediaServer\"
+copy ".\license.config" "C:\Program Files\Hanwha\Wisenet WAVE\MediaServer\"
+copy %BASE_DIR%"\s3.config" "C:\Program Files\Hanwha\Wisenet WAVE\MediaServer\"
+copy %BASE_DIR%"\s3_storage_plugin.dll" "C:\Program Files\Hanwha\Wisenet WAVE\MediaServer\plugins\"
 
 echo "Settingup sdk ..Done"
 
