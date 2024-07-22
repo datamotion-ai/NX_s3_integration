94c210668342f6f8720f2201cf572423ff537963
Brijal Panchal <brijalbenpanchal@loyalistcollege.com>
2024-07-22 10:56:53 -0400

    Hanwah OEM encryption

--- install-storage-sdk/install.sh
+++ install-storage-sdk/install.sh
@@ -8,7 +8,7 @@ echo "Settingup sdk .."
 systemctl stop hanwha-mediaserver.service
 
 apt-get install libcurl4-openssl-dev libssl-dev uuid-dev zlib1g-dev libpulse-dev
-./Storage_SDK_License_Config "6bc96cb9bb0a54d43615c4"
+./Storage_SDK_License_Config "78f73f8fa50a43801321ffbe"
 cp license.config /opt/hanwha/mediaserver/bin/
 cp s3.config /opt/hanwha/mediaserver/bin/
 cp libs3_storage_plugin.so /opt/hanwha/mediaserver/bin/plugins/
